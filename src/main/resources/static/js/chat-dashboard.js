efc388fbc16940df2087db074cf289038a8fc2bc
김동현 <zx12ehdgus12@gmail.com>
2025-03-17 20:47:44 +0900

    [DHTALK] * 1.0.2 * 채팅창 대시보드 CSS 수정

--- src/main/resources/static/js/chat-dashboard.js
+++ src/main/resources/static/js/chat-dashboard.js
@@ -32,9 +32,17 @@ function makeRoomList(rooms){
 
     rooms.forEach(function(room){
         let active = document.getElementById("selected-chat-room").value === room.roomId ? "active" : "";
-        roomHtml += `<li class="room-list ${active}" data-key="${room.roomId}" onclick="chatRoomSelected('${room.roomId}')">
-                        <span>${room.roomId}</span>
-                    </li>`;
+        roomHtml += `<div class="room-list ${active}" data-key="${room.roomId}" onclick="chatRoomSelected('${room.roomId}')">
+                        <div class="room-list-icon" data-icon="ChatCircleDots" data-size="24px" data-weight="regular">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+                                <path d="M140,128a12,12,0,1,1-12-12A12,12,0,0,1,140,128ZM84,116a12,12,0,1,0,12,12A12,12,0,0,0,84,116Zm88,0a12,12,0,1,0,12,12A12,12,0,0,0,172,116Zm60,12A104,104,0,0,1,79.12,219.82L45.07,231.17a16,16,0,0,1-20.24-20.24l11.35-34.05A104,104,0,1,1,232,128Zm-16,0A88,88,0,1,0,51.81,172.06a8,8,0,0,1,.66,6.54L40,216,77.4,203.53a7.85,7.85,0,0,1,2.53-.42,8,8,0,0,1,4,1.08A88,88,0,0,0,216,128Z"></path>
+                            </svg>
+                        </div>
+                        <div class="room-list-info">
+                            <p class="room-key">${room.roomId}</p>
+                            <p class="room-message">Room Message Preview</p>
+                        </div>
+                    </div>`;
     });
 
     roomListSection.innerHTML = roomHtml;
